92ac383a7512b2ef8adff089378228ac2860f11f
Leonardo Sousa <leonardofsousa@outlook.com>
2024-02-07 23:59:38 -0300

    feat: config form to send email

--- js/script.js
+++ js/script.js
@@ -10,8 +10,6 @@ menuIcon.onclick = () => {
 /*==================== toggle light/dark theme ====================*/
 let themeConfig = document.querySelector('#theme-config');
 
-console.log('themeConfig', themeConfig);
-
 themeConfig.onclick = () => {
     if (themeConfig.classList.toggle('bxs-moon')) {
         document.documentElement.setAttribute('data-theme', 'dark')
@@ -70,4 +68,109 @@ const typed = new Typed('.multiple-text', {
     typeSpeed: 200,
     backDelay: 100,
     loop: true
+});
+
+/*==================== smtp js ====================*/
+let form = document.querySelector('form');
+const fullname = document.getElementById('fullname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+function sendEmail() {
+    const bodyMessage =`Full Name: ${fullname.value}<br>Email: ${email.value}<br>Phone: ${phone.value}<br>Message: ${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "leonardofsousa@outlook.com",
+        Password : "B79F1778947BF57C649BA70364E0840DD357",
+        To : 'leonardofsousa@outlook.com',
+        From : "leonardofsousa@outlook.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK") {
+            swal({
+                title:"Success!", 
+                text:"Thanks! Your message has sent successfully!", 
+                icon: "success"
+            });
+        }
+        else {
+            swal({
+                title:"Oops", 
+                text:"Your message hasn't sent. Please try again." , 
+                icon:"error" 
+            });
+        }
+      }
+    );
+}
+
+/*==================== validate form js ====================*/
+function checkInputs() {
+    const items = document.querySelectorAll('.item');
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\_\-\.])+\@([a-z\d\-\_])+\.([a-z]{2,3})?$/;
+
+    const errorTxtEmail = document.querySelector(".error-msg.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email";
+        }
+        else {
+            errorTxtEmail.innerText = "Email address can't blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullname.classList.contains("error") &&
+        !email.classList.contains("error") &&
+        !phone.classList.contains("error") &&
+        !subject.classList.contains("error") &&
+        !message.classList.contains("error")) {
+            sendEmail();
+        }
 });
