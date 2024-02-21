a6d8eee30c8a88c751f8b46219f137ab5b8f75aa
Leonardo Sousa <leonardofsousa@outlook.com>
2024-02-21 00:29:32 -0300

    feat: add multi language support

--- js/script.js
+++ js/script.js
@@ -175,3 +175,63 @@ form.addEventListener("submit", (e) => {
             return false;
         }
 });
+
+
+/*==================== multiple language js ====================*/
+// const langObject = document.getElementById('linguagem');
+
+// Function to fetch language data
+async function fetchLanguageData(lang) {
+    const response = await fetch(`languages/${lang}.json`);
+    return response.json();
+  }
+  
+  // Function to set the language preference
+  function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+  }
+  
+  // Function to update content based on selected language
+  function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+      const key = element.getAttribute('data-i18n');
+      element.textContent = langData[key];
+    });
+  }
+  
+  // Function to change language
+  async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+    
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+  }
+
+  /*==================== toggle language config ====================*/
+let languageConfig = document.querySelector('#language-config');
+
+languageConfig.onclick = () => {
+    if (languageConfig.classList.toggle('fi-us')) {
+        changeLanguage('pt-br');
+        console.log('change pt-br');
+    }
+    else {
+        changeLanguage('en-us');
+        console.log('change en-us');
+    }
+}
+  
+  // Call updateContent() on page load
+  window.addEventListener('DOMContentLoaded', async () => {
+    console.log('DOMContentLoaded: ');
+    console.log('localStorage: ', localStorage);
+    const userPreferredLanguage = localStorage.getItem('language') || 'pt-br';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+
+    if (userPreferredLanguage == 'pt-br') {
+        languageConfig.classList.add('fi-us');
+    }else {
+        languageConfig.classList.add('fi-br');
+    }
+  });
